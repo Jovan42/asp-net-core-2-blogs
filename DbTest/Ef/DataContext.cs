7e87459fea942899087a1bd584e50753f390ce46
jovan0042 <j.manojlovic@vegaitsourcing.rs>
2019-05-16 09:18:46 +0200

    Add project files.

---
+++ DbTest/Ef/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DbTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbTest.Ef
+{
+    public class DataContext : DbContext
+    {
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Blog> Blog { get; set; }
+        public DbSet<Post> Post { get; set; }
+    }  
+}
