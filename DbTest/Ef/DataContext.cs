e495e4cba73ad822cd1c20143209fe7ab8a71eb0
jovan0042 <j.manojlovic@vegaitsourcing.rs>
2019-05-16 09:55:31 +0200

    Add CRUD for Posts

--- DbTest/Ef/DataContext.cs
+++ DbTest/Ef/DataContext.cs
@@ -9,7 +9,7 @@ namespace DbTest.Ef
 {
     public class DataContext : DbContext
     {
-    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Blog> Blog { get; set; }
         public DbSet<Post> Post { get; set; }
     }  
